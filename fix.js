9005f0eed3431092208d46a00edc0ac9e891edfc
sinan-prvt <mohamedsinan9400@gmail.com>
2026-04-26 22:29:11 +0530

    Updated

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.jsx', 'utf8');
-content = content.replace(/\\`/g, '`').replace(/\\\$/g, '$');
-fs.writeFileSync('src/App.jsx', content);
